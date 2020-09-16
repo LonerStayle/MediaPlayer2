3e35186ba0eeb74ee2f208a7808cf7b62b9e09ee
LonerStayle <60532838+LonerStayle@users.noreply.github.com>
2020-09-16 20:52:35 +0900

    [Feature]baseFragment 생성 및 설정 #3

--- app/src/main/java/com/example/a16mediaplayer/view/dest/MainFragment.kt
+++ app/src/main/java/com/example/a16mediaplayer/view/dest/MainFragment.kt
@@ -5,13 +5,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.a16mediaplayer.R
+import com.example.a16mediaplayer.base.BaseFragment
+import com.example.a16mediaplayer.databinding.FragmentMainBinding
 
-class MainFragment:Fragment() {
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+class MainFragment:BaseFragment<FragmentMainBinding>(R.layout.fragment_main) {
+    override fun FragmentMainBinding.bindingViewData() {
+        TODO("Not yet implemented")
     }
+
+    override fun FragmentMainBinding.setEventListener() {
+        TODO("Not yet implemented")
+    }
+
 }
--- app/src/main/java/com/example/a16mediaplayer/view/dest/PlayListFragment.kt
+++ app/src/main/java/com/example/a16mediaplayer/view/dest/PlayListFragment.kt
@@ -5,14 +5,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.a16mediaplayer.R
+import com.example.a16mediaplayer.base.BaseFragment
+import com.example.a16mediaplayer.databinding.FragmentPlayListBinding
 
-class PlayListFragment:Fragment() {
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        
-        return super.onCreateView(inflater, container, savedInstanceState)
+class PlayListFragment:BaseFragment<FragmentPlayListBinding>(R.layout.fragment_play_list) {
+    override fun FragmentPlayListBinding.bindingViewData() {
+        TODO("Not yet implemented")
     }
+
+    override fun FragmentPlayListBinding.setEventListener() {
+        TODO("Not yet implemented")
+    }
+
 }
